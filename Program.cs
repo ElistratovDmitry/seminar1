0a3556da5ce4380a7d6c8808628c646fa8ab7970
dmitry_el <dmitry_el@mail.ru>
2022-08-31 17:30:14 +0300

    задача 4 ДЗ

--- Program.cs
+++ Program.cs
@@ -1,10 +1,7 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
 
-
+Console.WriteLine("ЗАДАЧА 2 ДЗ");
 Console.WriteLine("введи число 1");
 int number1=int.Parse(Console.ReadLine());
 Console.WriteLine("введи число 2");
@@ -21,6 +18,16 @@ if (number1==number2) {
     Console.WriteLine($"первое число {number1} больше, а второе число {number2} меньше");
 }
 
-    
 
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
+ Console.WriteLine("ЗАДАЧА 4 ДЗ");   
+Console.WriteLine("введи число 1");
+int n1=int.Parse(Console.ReadLine());
+Console.WriteLine("введи число 2");
+int n2=int.Parse(Console.ReadLine());
+Console.WriteLine("введи число 3");
+int n3=int.Parse(Console.ReadLine());
+int maximum = Math.Max(n1, n2);
+maximum = Math.Max(maximum, n3);
+Console.WriteLine($"максимальное число {maximum}");
